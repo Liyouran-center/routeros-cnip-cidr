7abd6c1758006add105e9d54e64c7f0c4b23bc58
莫雨 <2681485724@qq.com>
2025-07-19 22:47:54 +0800

    Update generator.bash
    倒计时1小时删除

--- generator.bash
+++ generator.bash
@@ -12,12 +12,12 @@ cat > $WORK_DIR/dist/cn_ip_cidr.rsc << EOF
 /ip firewall address-list remove [/ip firewall address-list find list=cn_ip_cidr]
 /ip firewall address-list
 EOF
-cat $WORK_DIR/tmp/all_cn.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/dist/cn_ip_cidr.rsc && \
+cat $WORK_DIR/tmp/all_cn.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr timeout=86400 } on-error={}\n",$0) }' >> $WORK_DIR/dist/cn_ip_cidr.rsc && \
 cat >> $WORK_DIR/dist/cn_ip_cidr.rsc << EOF
 :if ([:len [/system package find where name="ipv6" and disabled=no]] > 0) do={
 /log info "Import cn ipv6 cidr list..."
 /ipv6 firewall address-list remove [/ipv6 firewall address-list find list=cn_ip_cidr]
 /ipv6 firewall address-list
 EOF
-cat $WORK_DIR/tmp/all_cn_ipv6.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/dist/cn_ip_cidr.rsc && \
+cat $WORK_DIR/tmp/all_cn_ipv6.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr timeout=86400 } on-error={}\n",$0) }' >> $WORK_DIR/dist/cn_ip_cidr.rsc && \
 echo "}" >> $WORK_DIR/dist/cn_ip_cidr.rsc
